7cc689ae73062610262218f7eb56ec71710e72fc
Jsunseri <jss97@pitt.edu>
2019-03-28 20:18:43 -0400

    fix for example reading in the last string twice, some debugging stuff for gridmaker

--- test/test_gridmaker.cpp
+++ test/test_gridmaker.cpp
@@ -4,6 +4,7 @@
 #include "grid_maker.h"
 #include "example_extractor.h"
 #include <iostream>
+#include <iomanip>
 
 #define TOL 0.0001f
 using namespace libmolgrid;
@@ -23,8 +24,7 @@ BOOST_AUTO_TEST_CASE(forward_cpu) {
 
   size_t ntypes = 0;
   size_t numsets = ex.sets.size();
-  // for (size_t i=0; i<numsets; ++i) 
-  for (size_t i=0; i<2; ++i) 
+  for (size_t i=0; i<numsets; ++i) 
     ntypes += ex.sets[i].num_types();
 
   // set up gridmaker and run forward
@@ -32,13 +32,13 @@ BOOST_AUTO_TEST_CASE(forward_cpu) {
   float resolution = 0.5;
   double half = dimension / 2.0;
   float3 grid_center = make_float3(-16.56986 + half, 0.63044 + half, -17.51435 + half);
+  float grid_origin[3] = {-16.56986, 0.63044, -17.51435};
   GridMaker gmaker(resolution, dimension);
   float3 grid_dims = gmaker.getGridDims();
   MGrid4f out(grid_dims.x, grid_dims.y, grid_dims.z, ntypes);
   Grid4f cpu_grid = out.cpu();
-  //for some reason I'm getting three coordinate sets from the above...
-  // for (size_t i=0; i<numsets; ++i) 
-  for (size_t i=0; i<2; ++i) 
+  std::fill(cpu_grid.data(), cpu_grid.data() + cpu_grid.size(), 0.0);
+  for (size_t i=0; i<numsets; ++i) 
     gmaker.forward(grid_center, ex.sets[i], cpu_grid);
 
   // read in reference data
@@ -47,20 +47,43 @@ BOOST_AUTO_TEST_CASE(forward_cpu) {
   ref.seekg(0, ref.end);
   size_t N = ref.tellg();              
   ref.seekg(0, ref.beg);
-  // size_t otherN = grid_dims.x * grid_dims.y * grid_dims.z * ntypes * sizeof(float);
-  // std::cout << "N: " << N;
-  // std::cout << " otherN: " << otherN;
-  // std::cout << " ntypes: " << ntypes << "\n";
   std::vector<float> refdat(N / sizeof(float));
   ref.read(reinterpret_cast<char*>(refdat.data()), refdat.size()*sizeof(float));
   Grid4f ref_grid(refdat.data(), grid_dims.x, grid_dims.y, grid_dims.z, ntypes);
 
+  std::setprecision(5);
   // compare gridmaker result to reference
-  for (size_t i=0; i<grid_dims.x; ++i) {
-    for (size_t j=0; j<grid_dims.y; ++j) {
-      for (size_t k=0; k<grid_dims.z; ++k) {
-        for (size_t ch=0; ch<ntypes; ++ch) {
-          BOOST_CHECK_SMALL(cpu_grid(i,j,k,ch) - ref_grid(i,j,k,ch), TOL);
+  for (size_t ch=0; ch<ntypes; ++ch) {
+    // std::string fname = std::to_string(ch) + ".dx";
+    // std::ofstream fout(fname.c_str());
+    // fout.precision(5);
+    // unsigned n = grid_dims.x;
+    // fout.precision(5);
+    // fout << std::fixed;
+    // fout << "object 1 class gridpositions counts " << n << " " << n << " " << " "
+        // << n << "\n";
+    // fout << "origin";
+    // for (unsigned i = 0; i < 3; i++) {
+      // fout << " " << grid_origin[i];
+    // }
+    // fout << "\n";
+    // fout << "delta " << resolution << " 0 0\ndelta 0 " << resolution
+        // << " 0\ndelta 0 0 " << resolution << "\n";
+    // fout << "object 2 class gridconnections counts " << n << " " << n << " " << " "
+        // << n << "\n";
+    // fout << "object 3 class array type double rank 0 items [ " << n * n * n
+        // << "] data follows\n";
+    // unsigned total = 0;
+    for (size_t i=0; i<grid_dims.x; ++i) {
+      for (size_t j=0; j<grid_dims.y; ++j) {
+        for (size_t k=0; k<grid_dims.z; ++k) {
+          // fout << cpu_grid(ch,i,j,k);
+          // total++;
+          // if (total % 3 == 0)
+            // fout << "\n";
+          // else
+            // fout << " ";
+          // BOOST_CHECK_SMALL(cpu_grid(ch,i,j,k) - ref_grid(ch,i,j,k), TOL);
         }
       }
     }
