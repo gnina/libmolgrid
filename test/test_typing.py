585484f694303ea033b85e1fa2f5c0245faa7267
David Koes <dkoes@pitt.edu>
2019-06-04 15:20:50 -0400

    Make compat with OB 3.0
    Darn you Noel

--- test/test_typing.py
+++ test/test_typing.py
@@ -382,7 +382,10 @@ def test_callbackvector_typing():
     m.addh()
 
     def mytyper(atom):
-        return ([atom.GetAtomicNum(),atom.GetValence()], 1.5)
+        if hasattr(atom, 'GetValence'):
+            return ([atom.GetAtomicNum(),atom.GetValence()], 1.5)
+        else:
+            return ([atom.GetAtomicNum(),atom.GetExplicitDegree()], 1.5)
     
     t = molgrid.PythonCallbackVectorTyper(mytyper, 2, ["anum","valence"])
     typs = [t.get_atom_type_vector(a.OBAtom) for a in m.atoms]
