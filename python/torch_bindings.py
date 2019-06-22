0f9f6c240e777a6ecc097969df9ad18e31b3d0a5
David Koes <dkoes@pitt.edu>
2019-06-22 13:53:11 -0400

    Torch bindings for Coord2Grid

--- python/torch_bindings.py
+++ python/torch_bindings.py
@@ -36,19 +36,44 @@ def make_grid_tensor(gridmaker, center, c):
 
 mg.GridMaker.make_tensor = make_grid_tensor
     
-class Coords2GridFunction(torch.Function):
+class Coords2GridFunction(torch.autograd.Function):
     '''Layer for converting from coordinate and type tensors to a molecular grid'''
     
     @staticmethod
-    def forward(ctx, gmaker, coords, types, radii):
+    def forward(ctx, gmaker, center, coords, types, radii):
         '''coords are Nx3, types are NxT, radii are N'''
         ctx.save_for_backward(coords, types, radii)
         ctx.gmaker = gmaker
-        #gmaker.forward()
-        
-        #will need to create output tensor to view as grid
+        ctx.center = center
+        shape = gmaker.grid_dimensions(types.shape[1]) #ntypes == nchannels
+        output = torch.empty(*shape,dtype=torch.float32,device=coords.device)
+        gmaker.forward(center, coords, types, radii, output)
+        return output
         
     @staticmethod
     def backward(ctx, grid_gradient):
+        '''Return Nx3 coordinate gradient and NxT type gradient'''
         coords, types, radii = ctx.saved_tensors
         gmaker = ctx.gmaker
+        center = ctx.center
+        grad_coords = torch.empty(*coords.shape,dtype=coords.dtype,device=coords.device)
+        grad_types = torch.empty(*types.shape,dtype=types.dtype,device=types.device)
+        #radii are fixed
+        gmaker.backward(center, coords, types, radii, grid_gradient, grad_coords, grad_types)
+        return None, None, grad_coords, grad_types, None
+        
+        
+class Coords2Grid(torch.nn.Module):
+    def __init__(self, gmaker, center):
+        '''Convert coordinates/types/radii to a grid using the provided
+        GridMaker and grid center'''
+        super(Coords2Grid, self).__init__()
+        self.gmaker = gmaker
+        self.center = center
+        
+    def forward(self, coords, types, radii):
+        return Coords2GridFunction.apply(self.gmaker, self.center, coords, types, radii)
+    
+    def extra_repr(self):
+        return 'resolution {:.2f}, dimension {}, center {:.3f},{:.3f},{:.3f}'.format(
+                self.gmaker.get_resolution(), self.gmaker.get_dimension(), self.center[0], self.center[1], self.center[2])        
--- test/test_torch.py
+++ test/test_torch.py
@@ -2,7 +2,7 @@ import pytest
 import molgrid
 import torch
 import os
-from molgrid import tensor_as_grid
+from molgrid import tensor_as_grid, Coords2GridFunction
 from pytest import approx
 
 '''Test ability to manipulate data in torch tensors'''
@@ -121,3 +121,25 @@ def test_torch_gnina_example_provider():
 
     for i in range(nlabels):
         assert label0[i] == labels[i][0]
+
+def test_function():
+    for dev in ('cuda','cpu'):
+        gmaker = molgrid.GridMaker(resolution=.1,dimension=6.0)
+        c = torch.tensor([[1.0,0,0],[1,0,0]],device=dev,dtype=torch.float32,requires_grad=True)
+        vt = torch.tensor([[0,1.0,0],[1.0,0,0]],device=dev,dtype=torch.float32,requires_grad=True)
+        r = torch.tensor([2.0,2.0],device=dev,dtype=torch.float32)
+        
+        grid = Coords2GridFunction.apply(gmaker, (0,0,0), c, vt, r)
+    
+        shape = gmaker.grid_dimensions(3)    
+        #make diff with gradient in center
+        diff = torch.zeros(*shape,dtype=torch.float32,device=dev)
+        diff[0,30,30,30] = 1.0  
+        diff[1,30,30,30] = -1.0  
+            
+        grid.backward(diff)
+        assert c.grad[0].cpu().numpy() == approx([0.60653,0,0],abs=1e-4)
+        assert c.grad[1].cpu().numpy() == approx([-0.60653,0,0],abs=1e-4)
+        
+        assert vt.grad[0].cpu().numpy() == approx([0.60653,-0.60653,0],abs=1e-4)
+        assert vt.grad[1].cpu().numpy() == approx([0.60653,-0.60653,0],abs=1e-4)
