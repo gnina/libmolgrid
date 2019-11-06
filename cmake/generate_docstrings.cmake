4277041803bccc5f5487c7cef05a521f125b6958
Jsunseri <jss97@pitt.edu>
2019-11-05 19:56:23 -0500

    cmake script generates reasonable docstrings automatically

--- cmake/generate_docstrings.cmake
+++ cmake/generate_docstrings.cmake
@@ -1,34 +1,25 @@
 # function to convert Doxygen-style comments into Python-style docstrings
-function(doxyToDoc DOCSTRING DOXYSTRING)
-  string(REGEX REPLACE "\brief " "" DOCSTRING "${DOXYSTRING}")
-  if("${DOXYSTRING}" MATCHES "@param ([a-zA-Z0-9_]+) (.*)")
+function(doxyToDoc DOXYSTRING)
+  message(STATUS "got line " "${DOXYSTRING}")
+  string(REGEX REPLACE "[ \t]*/\\*+[ \t]*\\\\brief[ \t]*" "" DOXYSTRING "${DOXYSTRING}")
+  if("${DOXYSTRING}" MATCHES "[ \t]*\\*[ \t]*@param[ \t]*(\\[[a-zA-Z0-9_]+\\]) (.*)")
     set(DOXYSTRING ":param ${CMAKE_MATCH_1}: ${CMAKE_MATCH_2}")
-  endif()
-  if ("${DOXYSTRING}" MATCHES "@return (.+)")
+  elseif ("${DOXYSTRING}" MATCHES "[ \t]*\\*[ \t]*@return (.+)")
     set(DOXYSTRING ":returns: ${CMAKE_MATCH_1}")
+  elseif("${DOXYSTRING}" MATCHES "[ \t]*\\*/*(.*)")
+    set(DOXYSTRING "${CMAKE_MATCH_1}")
   endif()
-  set(DOCSTRING ${DOCSTRING} ${DOXYSTRING} PARENT_SCOPE)
+  message(STATUS "doxystring is " "${DOXYSTRING}")
+  set(DOCSTRING "${DOCSTRING}" "${DOXYSTRING}" PARENT_SCOPE)
 endfunction()
 
-# automatically generate docstrings in bindings from Doxygen annotations in cpp header files
-file(READ "bindings.cpp.in" CONTENTS)
-STRING(REGEX REPLACE ";" "\\\\;" CONTENTS "${CONTENTS}")
-string(REGEX REPLACE "\n" ";" CONTENTS "${CONTENTS}")
-
-# make list of object names for retrieval later
-foreach(LINE ${CONTENTS})
-  if("${LINE}" MATCHES ".*\"@(Docstring_[a-zA-Z0-9_]+)@\"\);")
-    set(OBJS ${OBJS} ${CMAKE_MATCH_1})
-  endif()
-endforeach()
-
 # find the headers
-file(GLOB HEADERS  ${PROJECT_SOURCE_DIR}/include/libmolgrid)
+file(GLOB HEADERS  ${PROJECT_SOURCE_DIR}/include/libmolgrid/*.h)
 
 # make map of object name to docstring content
 set(COPYING "0")
 foreach(fname ${HEADERS})
-  file(READ fname HEADER_CONTENTS)
+  file(READ ${fname} HEADER_CONTENTS)
   STRING(REGEX REPLACE ";" "\\\\;" HEADER_CONTENTS "${HEADER_CONTENTS}")
   string(REGEX REPLACE "\n" ";" HEADER_CONTENTS "${HEADER_CONTENTS}")
   foreach(LINE ${HEADER_CONTENTS})
@@ -37,17 +28,20 @@ foreach(fname ${HEADERS})
       set(COPYING "1")
       set(FUNC ${CMAKE_MATCH_1})
     # continuing extraction of previous comment
-    elseif(${COPYING} AND "${LINE}" MATCHES "(/\*|\*)([\*]+)([a-zA-Z0-9@\\\.\(\)]+)")
-      doxyToDoc(DOCSTRING LINE)
-    # insert into map, zero out string
     elseif(${COPYING})
-      string(REPLACE ";" "\\n" DOCSTRING "${DOCSTRING}")
-      set(${FUNC} ${DOCSTRING})
-      set(COPYING "0")
-      set(DOCSTRING "")
+      if("${LINE}" MATCHES "([ \t]*)(/\\*|\\*)(\\**)(.*)")
+        doxyToDoc("${LINE}")
+      else()
+        # insert into map, zero out string
+        string(REPLACE ";" "\\n" DOCSTRING "${DOCSTRING}")
+        set(${FUNC} ${DOCSTRING})
+        message(STATUS "function " "${FUNC}" " has docstring " "${DOCSTRING}")
+        set(COPYING "0")
+        set(DOCSTRING "")
+      endif()
     endif()
   endforeach()
 endforeach()
 
 # insert docstrings into bindings
-configure_file("bindings.cpp.in" "bindings.cpp" @ONLY)
+configure_file("${PROJECT_SOURCE_DIR}/python/bindings.cpp.in" "${PROJECT_SOURCE_DIR}/python/bindings.cpp" @ONLY)
