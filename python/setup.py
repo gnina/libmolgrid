838e8b6a25951551e2d75360db2155e98b21684a
Jsunseri <jss97@pitt.edu>
2019-11-19 14:08:11 -0500

    changes to setup.py

--- python/setup.py
+++ python/setup.py
@@ -1,14 +1,60 @@
-from distutils.core import setup
+try:
+        from setuptools import setup
+except ImportError:
+        from distutils.core import setup
+import sys,os,subprocess
 
-import sys
+def git_pep440_version(path):
+    def git_command(args):
+        prefix = ['git', '-C', path]
+        return subprocess.check_output(prefix + args).decode().strip()
+    version_full = git_command(['describe', '--tags', '--dirty=.dirty'])
+    version_tag = git_command(['describe', '--tags', '--abbrev=0'])
+    version_tail = version_full[len(version_tag):]
+    return version_tag + version_tail.replace('-', '.dev', 1).replace('-', '+', 1)
+
+try:
+    version = git_pep440_version(os.path.dirname(os.path.realpath(__file__)))
+except subprocess.CalledProcessError:
+    sys.exit('Cannot obtain version number from git.')
+
 if sys.version_info < (2,7):
   sys.exit('Sorry, Python < 2.7 is not supported')
 
 setup(
   name        = 'molgrid',
-  version     = '${PACKAGE_VERSION}', # TODO: might want to use commit ID here
+  description='Grid-based molecular modeling library',
+  long_description='molgrid can be used to generate several types of tensors '
+  'from input molecules, most uniquely three-dimensional voxel grids. Input can '
+  'be specified fairly flexibly, with native support for numpy arrays and torch '
+  'tensors as well as major molecular file formats via OpenBabel. Output '
+  'generation has several options that facilitate obtaining good performance '
+  'from machine learning algorithms, including features like data augmentation '
+  'and resampling.',
+  version     = version, 
   author='David Ryan Koes and Jocelyn Sunseri',
   author_email='dkoes@pitt.edu',
+  classifiers=[
+      'Development Status :: 4 - Beta',
+
+      'Intended Audience :: Developers',
+      'Intended Audience :: Science/Research',
+      'Topic :: Scientific/Engineering :: Chemistry',
+
+      'License :: OSI Approved :: Apache License',
+      'License :: OSI Approved :: GPL3 License',
+  
+      'Programming Language :: C++',
+      'Programming Language :: Python :: 3',
+  ],
+  url = 'https://github.com/gnina/libmolgrid',
+  project_urls = {'Documentation': 'http://gnina.github.io/libmolgrid'},
+  install_requires = [
+                 'numpy',
+                 'pytest',
+                 'pyquaternion'
+      ],
+  python_requires = '>=3',
   packages    = ['molgrid'],
   package_dir = {
     '': '${CMAKE_CURRENT_BINARY_DIR}'
